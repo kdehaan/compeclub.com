b776e36cfc73c2db828838959af4f993fc892615
Kevin de Haan <kbehaan@gmail.com>
2019-08-08 19:31:05 -0600

    Updates

---
+++ precache-manifest.982ef35a48853c9ee7063cea36d273d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ab1fb3358b9b71afd265c1b0379807",
+    "url": "/compeclub.com/index.html"
+  },
+  {
+    "revision": "610be890fef805fbf267",
+    "url": "/compeclub.com/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/compeclub.com/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "610be890fef805fbf267",
+    "url": "/compeclub.com/static/js/main.8745bcf6.chunk.js"
+  },
+  {
+    "revision": "b3d2817f91734dda499c",
+    "url": "/compeclub.com/static/js/runtime~main.82420e37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/compeclub.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
