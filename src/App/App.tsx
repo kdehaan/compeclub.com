d436d708c5f8ddde21697f6bb50d58ca0cb4d9de
Kevin de Haan <kbehaan@gmail.com>
2019-08-08 20:19:06 -0600

    added material ui

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
 const App: React.FC = () => {
-  return <React.Fragment />;
+  return (
+    <React.Fragment>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </React.Fragment>
+  );
 };
 
 export default App;
